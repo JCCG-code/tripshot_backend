9b2d41574c29d0cb505bdddcd7409d646f3c69a5
Juan Carlos Cuadrado Gracia <juan-carlos.cuadrado-gracia@t-systems.com>
2024-04-04 12:28:58 +0200

    config

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-// src/index.js
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Routes
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
-});
+// Initializations
+const app = express()
+const port = process.env.PORT
 
+// Middewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  cors({
+    origin: (callback) => callback(null, true),
+    credentials: true
+  })
+)
+
+// Routes
+
+// Server and database are listening
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(console.log('[server] Mongoose connection has been done successfully'))
+  .catch((err) => console.log(err))
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`[server] Server is running at http://localhost:${port}`)
+})
